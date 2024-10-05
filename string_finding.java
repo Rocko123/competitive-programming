8585a9309de7dd7d3cc1d70db7e7943bfeaa39d0
Rocko123 <115847633+Rocko123@users.noreply.github.com>
2024-10-05 19:08:37 -0400

    solved

--- string_finding.java
+++ string_finding.java
@@ -6,19 +6,25 @@ public class string_finding {
     public static void main(String[] args) throws IOException{
         // kmp algorithm
         String s = readLine(), t = readLine();
+        String prefix = t+"A"+s;
 
-        int[] lps = new int[s.length()];
-        for (int i = 1; i < s.length(); i++) {
+        int[] lps = new int[prefix.length()];
+        for (int i = 1; i < prefix.length(); i++) {
             int j = lps[i-1]; // longest prefix of guy before you
-            while (j > 0 && s.charAt(i) != s.charAt(j)) j = lps[j-1]; // find the first equal one before you
-            if (s.charAt(i) == s.charAt(j)) j++;
+            while (j > 0 && prefix.charAt(i) != prefix.charAt(j)) j = lps[j-1]; // find the first equal one before you
+            if (prefix.charAt(i) == prefix.charAt(j)) j++;
             // since length is 1 idx, j is actually the string idx+1, if i is that value
             // then you can extend the lps
             lps[i] = j;
         }
-        // https://cp-algorithms.com/string/prefix-function.html
-        // https://www.youtube.com/watch?v=V5-7GzOfADQ
-
+        //System.out.println(Arrays.toString(lps));
+        for (int i = t.length()+1; i < prefix.length(); i++) {
+            if (lps[i] == t.length()) {
+                System.out.println(i-2*t.length());
+                System.exit(0);
+            }
+        }
+        System.out.println(-1);
     } 
     static String next () throws IOException {
         while (st == null || ! st.hasMoreTokens())
